b7465a83497a81e15849893e188dace76ca4b520
Hanh Vo <hanhvo@hanhs-macbook-pro.local>
2016-01-06 11:34:23 +0700

    [Implementation] Create Project Structure.

---
+++ ForthTV/ForthTV/02.Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ForthTV
+//
+//  Created by Hanh Vo on 1/4/16.
+//  Copyright © 2016 Hanh Vo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
