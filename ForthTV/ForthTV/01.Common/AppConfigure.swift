ab6ec37a88b8a1f3f1e23bdc3b43a9d404cda0b0
Hanh Vo <hanhvo@hanhs-macbook-pro.local>
2016-01-06 12:05:51 +0700

    [Implementation] Create Project Structure.

--- ForthTV/ForthTV/01.Common/AppConfigure.swift
+++ ForthTV/ForthTV/01.Common/AppConfigure.swift
@@ -7,3 +7,22 @@
 //
 
 import Foundation
+
+// StoryBoards
+let MASTER_PAGE_STORYBOARD:String = "MasterController"
+let WELCOME_PAGE_STORYBOARD:String = "WelcomeController"
+let MENU_PAGE_STORYBOARD:String = "MenuController"
+let VIDEO_LIST_STORYBOARD:String = "VideoListController"
+let SLIDE_CONTAINER_STORYBOARD:String = "SlideContainerController"
+let ACCOUNT_STORYBOARD:String = "AccountController"
+let LOGIN_STORYBOARD:String = "LoginController"
+let REGISTER_STORYBOARD:String = "RegisterController"
+let VIDEO_DETAIL_STORYBOARD:String = "VideoDetailController"
+let CHATTING_STORYBOARD:String = "ChattingController"
+let BROACAST_STORYBOARD:String = "BroadcastController"
+let RECORD_STORYBOARD:String = "RecordController"
+let USER_PROFILE_STORYBOARD:String = "UserProfileController"
+let VISITTER_PROFILE_STORYBOARD:String = "VisitterProfileController"
+
+
+
