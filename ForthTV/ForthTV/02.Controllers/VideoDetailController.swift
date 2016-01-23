06ddc330db172bb41f32159ee386ae39280452e6
Hanh Vo <hanh.vo@sugar.sg>
2016-01-23 10:51:23 +0700

    Implementation: Video detail.

--- ForthTV/ForthTV/02.Controllers/VideoDetailController.swift
+++ ForthTV/ForthTV/02.Controllers/VideoDetailController.swift
@@ -9,27 +9,50 @@
 import UIKit
 
 class VideoDetailController: BaseController {
-
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var videoView: UIView!
+   
+    @IBOutlet weak var titleView: UIView!
+    @IBOutlet weak var imgAvatar: UIImageView!
+    @IBOutlet weak var gameChannel: UILabel!
+    
+    
+    @IBOutlet weak var centerView: UIView!
+    @IBOutlet weak var viewLabel: UILabel!
+    @IBOutlet weak var likeLabel: UILabel!
+    
+    
+    @IBOutlet weak var chatView: UIView!
+    @IBOutlet weak var chatTable: UITableView!
+    @IBOutlet weak var messageTextfield: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var emotionButton: UIButton!
+    
+    
+    //MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //MARK: Actions
+    
+    @IBAction func likeTapped(sender: AnyObject) {
     }
-    */
-
+    
+    @IBAction func sendMessageTapped(sender: AnyObject) {
+    }
+    
+    @IBAction func emotionTapped(sender: AnyObject) {
+    }
+    
+    
 }
